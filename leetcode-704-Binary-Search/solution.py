cf3b31d51036be5863a71ad09c80acb6edba140a
rtn75000 <100521999+rtn75000@users.noreply.github.com>
2022-05-12 21:43:58 +0300

    Update solution.py

--- leetcode-704-Binary-Search/solution.py
+++ leetcode-704-Binary-Search/solution.py
@@ -1,15 +1,21 @@
-"""Binary search #TC O(log n) 
-source : https://leetcode.com/problems/binary-search/  """
+""" # binary search #TC O(log n) #SC O(1) """
 
 class Solution:
     def search(self, arr: List[int], target: int) -> int:
-        left,right=0,len(arr)-1        
+        
+        left,right=0,len(arr)-1    
+        
         while left<=right:
-            mid =(left+right)//2 
+            
+            mid =(left+right)//2   # le pivot va etre le milieu
+            
             if arr[mid]==target :
-                return mid 
-            elif arr[mid]< target :
+                return mid         # retour de l'index du target
+            
+            elif arr[mid]< target :   # cad le target ce trouve a droite du mid 
                 left=mid+1 
-            else: 
+                
+            else:                     # cad le target ce trouve a gauche du mid
                 right = mid-1
-        return -1
+                
+        return -1   # si target pas trouver retourne -1
